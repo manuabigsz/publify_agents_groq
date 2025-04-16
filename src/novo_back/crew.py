dc0d3b25289d548d18bb3609a3ceaa1ad3fa349b
manuabigsz <manuelaossanes@hotmail.com>
2025-04-16 09:26:29 -0300

    fix: agent

--- src/novo_back/crew.py
+++ src/novo_back/crew.py
@@ -56,8 +56,7 @@ class NovoBack():
         return Crew(
             agents=self.agents, # Automatically created by the @agent decorator
             tasks=self.tasks, # Automatically created by the @task decorator
-            process=Process.sequential,
-            verbose=True,
-            memory=True
+            process=Process.sequential
+          
             # process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
         )
